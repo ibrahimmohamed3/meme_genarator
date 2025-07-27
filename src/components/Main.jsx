354f45239e675d7d3e2c9cfe39d7a6ae71d7c305
ibrahim <ibra39167271@gmail.com>
2025-07-27 21:47:33 +0300

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+
+export default function Main() {
+    const [meme, setMeme] = useState({
+       topText: "One does not simply",
+       bottomText: "Walk into Mordor",
+       imageUrl:"http://i.imgflip.com/1bij.jpg"
+    })
+/**
+         * Challenge: update the topText value in the meme state
+         * object every time the topText input box is changed
+         * 
+         * Note: don't worry about bottomText at this point.
+         */
+  function handleChange(event){
+    const {value, name} = event.currentTarget
+    setMeme(prevMeme=>({...prevMeme, 
+        [name]: value}))
+    
+  }
+  
+
+  
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value = {meme.topText}
+                       
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value = {meme.bottomText}
+                        
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
